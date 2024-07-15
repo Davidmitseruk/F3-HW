26755e2b2a370618858b05d4d97be2bcbaee5ec5
Davidmitseruk <davidmitseruk@gmail.com>
2024-07-15 15:13:06 +0200

    lol

---
+++ hw-8/script.js
@@ -0,0 +1,26 @@
+//1
+
+const friends = ['Mango', 'Poly', 'Kiwi', 'Ajax'];
+console.log(friends.join(', '))
+
+//2
+
+const cards = [
+    'card-1',
+    'card-2',
+    'card-3',
+    'card-4',
+    'card-5'
+]
+
+//3
+
+console.log(cards.indexOf(2));
+
+//4
+
+console.log(cards.push('card-6'))
+
+//5
+
+console.log(cards.splice(4))
