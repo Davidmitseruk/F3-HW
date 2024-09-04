4dc7afeea3738db4f29988fe48972b3b4bffe888
Davidmitseruk <davidmitseruk@gmail.com>
2024-09-04 15:24:28 +0200

    lol

--- hw-17/script.js
+++ hw-17/script.js
@@ -16,6 +16,19 @@ imgThree.style.width = "400px";
 console.log(imgThree);
 
 // 4
-const list = document.querySelector('#listFour');
-const firstItem = list.firstElementChild;
-firstItem.textContent = 'newTextContent'
+let counterValue = 0;
+const valueElement = document.querySelector('#value');
+const incrementButton = document.querySelector('[data-action="increment"]');
+const decrementButton = document.querySelector('[data-action="decrement"]');
+function increment() {
+  counterValue += 1; // Збільшення значення на 1
+  updateUI()}; // Оновлення інтерфейсу
+  function decrement() {
+    counterValue -= 1; // Зменшення значення на 1
+    updateUI(); // Оновлення інтерфейсу
+  }
+  function updateUI() {
+    valueElement.textContent = counterValue; // Встановлення нового значення в елементі span
+  }
+  incrementButton.addEventListener('click', increment);
+decrementButton.addEventListener('click', decrement);
